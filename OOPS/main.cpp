14625b05652a9a309366a8acc0d28be0de5aabfb
Sunraj <sunrajpatelcph@gmail.com>
2024-06-03 04:50:43 +0530

    Fixed implementation

--- OOPS/main.cpp
+++ OOPS/main.cpp
@@ -1,306 +1,362 @@
-//bank cli app using oops
-//user class with all the functions
-//bank account class which will have username  as parameter
-//admin class which will have all the functions
-
 #include <iostream>
 #include <string>
 #include <vector>
-#include <fstream>
-#include <sstream>
 #include <iomanip>
 #include <algorithm>
 #include <ctime>
+#include <sstream>
 
 using namespace std;
 
-struct transaction
-{
-	string type;
-	double amount;
-	string date;
-	transaction(string type, double amount, string date)
-	{
-		this->type = type;
-		this->amount = amount;
-		this->date = date;
-	}
-};
+struct transaction {
+    string type;
+    double amount;
+    string date;
 
-class bankAccount
-{ //variables under bankAccount should be accountNumber, balance, opendate, type which can be savings or current, and a vector of transactions for transaction history
-//functions should deposit(), withdraw(), transfer()
-private:
-	string accountNumber;
-	double balance;
-	string openDate;
-	string type;
-	vector<transaction> transactions;
-public:
-	bankAccount(string accountNumber, double balance, string openDate, string type)
-	{
-		this->accountNumber = accountNumber;
-		this->balance = balance;
-		this->openDate = openDate;
-		this->type = type;
-	}
-	void deposit(double amount)
-	{
-		balance += amount;
-		transaction t("Deposit", amount, date());
-		transactions.push_back(t);
-	}
-	void withdraw(double amount)
-	{
-		if (balance - amount < 0)
-		{
-			std::cout << "Insufficient funds" << endl;
-		}
-		else
-		{
-			balance -= amount;
-			transaction t("Withdraw", amount, date());
-			transactions.push_back(t);
-		}
-	}
-	void transfer(double amount, bankAccount &receiver)
-	{
-		if (balance - amount < 0)
-		{
-			std::cout << "Insufficient funds" << std::endl;
-		}
-		else
-		{
-			balance -= amount;
-			receiver.deposit(amount);
-			transaction t("Transfer", amount, date());
-			transactions.push_back(t);
-		}
-	}
-	int getBalance()
-	{
-		return balance;
-	}
-	string getAccountNumber()
-	{
-		return accountNumber;
-	}
-	string getType()
-	{
-		return type;
-	}
-	size_t getTransactionsSize() const {
-        return transactions.size();
+    transaction(string type, double amount, string date) {
+        this->type = type;
+        this->amount = amount;
+        this->date = date;
     }
-	string getTransactionType(int index){
-		return transactions[index].type;
-	}
-	double getTransactionAmount(int index){
-		return transactions[index].amount;
-	}
-	string getTransactionDate(int index){
-		return transactions[index].date;
-	}
-	void addTransaction(transaction t)
-	{
-		transactions.push_back(t);
-	}
-	virtual void addInterest(int days) = 0;
-	string date() {
-		auto t = std::time(nullptr);
-		auto timePtr = std::localtime(&t);
-
-		std::ostringstream oss;
-		oss << std::put_time(timePtr, "%d-%m-%Y");
-		return oss.str();
-	}
 };
 
-class bankAccountHolder
-{//use private and public to make the variables and functions private and public
-//variables under user should be name, username, netbanking password and list of bankAccounts under this user
+class bankAccount {
 private:
-	string name;
-	string username;
-	string netBankingPassword;
-	vector<bankAccount> bankAccounts;
+    string accountType;
+    string accountNumber;
+    double balance;
+    string openDate;
+    string type;
+    vector<transaction> transactions;
+    int interestRate;
 public:
-	bankAccountHolder(string name, string username, string netBankingPassword)
-	{
-		this->name = name;
-		this->username = username;
-		this->netBankingPassword = netBankingPassword;
-	
-	}
-	//functions should be getAccounts(), changePassword() and createAccount()
-	void listAccounts()
-	{
-		//print with widths of 20 and pipes running through the columns
-		std::cout << left << setw(20) << "Account Number" << "|" << setw(20) << "Balance" << "|" << setw(20) << "Type" << "|" << endl;
-		std::cout << "-----------------------------------------------" << endl;
-		for (int i = 0; i < bankAccounts.size(); i++)
-		{
-			std::cout << left << setw(20) << bankAccounts[i].getAccountNumber() << "|" << setw(20) << bankAccounts[i].getBalance() << "|" << setw(20) << bankAccounts[i].getType() << "|" << endl;
-		}
-	}
-	void createAccount(string accountNumber, double balance, string openDate, string type)
-	{
-		//create a new derived class object and add it to the bankAccounts vector
-		savingsAccount b(accountNumber, balance, openDate); // Remove the 'type' argument
-		bankAccounts.push_back(b);
-	}
+    bankAccount(string accountNumber, double balance, string openDate, string type, int interestRate, string accountType) {
+        this->accountNumber = accountNumber;
+        this->balance = balance;
+        this->openDate = openDate;
+        this->type = type;
+        this->interestRate = interestRate;
+        this->accountType = accountType;
+    }
+    string getAccountNumber() {
+        return accountNumber;
+    }
+    string getAccountType() {
+        return accountType;
+    }
+    double getBalance() {
+        return balance;
+    }
+    string getOpenDate() {
+        return openDate;
+    }
+    string getType() {
+        return type;
+    }
+    int getInterestRate() {
+        return interestRate;
+    }
+    vector<transaction> getTransactions() {
+        return transactions;
+    }
+    void changeInterestRate(int rate) {
+        if (accountType == "Current") {
+            cout << "Interest rate cannot be changed for current account" << endl;
+            return;
+        }
+        interestRate = rate;
+    }
+    void deposit(double amount) {
+        balance += amount;
+        transactions.push_back(transaction("Deposit", amount, date()));
+    }
+    void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            transactions.push_back(transaction("Withdraw", amount, date()));
+        }
+        else {
+            cout << "Insufficient balance" << endl;
+        }
+    }
+
+    void viewTransactions() {
+        cout << left << setw(20) << "Transaction Type" << setw(20) << "Amount" << setw(20) << "Date" << endl;
+        for (int i = 0; i < transactions.size(); i++) {
+            cout << left << setw(20) << transactions[i].type << setw(20) << transactions[i].amount << setw(20) << transactions[i].date << endl;
+        }
+    }
+    string date() {
+        auto t = time(nullptr);
+        auto tmPtr = localtime(&t);
+        ostringstream oss;
+        oss << std::put_time(tmPtr, "%d-%m-%Y");
+        return oss.str();
+    }
 };
 
-
-
-class branchManager
-{//variables under admin should be username and password
-//methods should getStatementOfAccount(), getAccountHolders(), fastforward()
+class bankAccountHolder {
 private:
-	string username;
-	string password;
+    string name;
+    string username;
+    string password;
+    vector<bankAccount> accounts;
 public:
-	branchManager(string username, string password)
-	{
-		this->username = username;
-		this->password = password;
-	}
-	void getStatementOfAccount(bankAccount &account)
-	{
-		std::cout << left << setw(20) << "Type" << "|" << setw(20) << "Amount" << "|" << setw(20) << "Date" << "|" << endl;
-		std::cout << "-----------------------------------------------" << endl;
-		for (int i = 0; i < account.getTransactionsSize(); i++)
-		{
-			std::cout << left << setw(20) << account.getTransactionType(i) << "|" << setw(20) << account.getTransactionAmount(i) << "|" << setw(20) << account.getTransactionDate(i) << "|" << endl;
-		}
-	}
-	string getName() {
-		return name;
-	}
-	void getAccountHolders(vector<bankAccountHolder> &accountHolders)
-	{
-		std::cout << left << setw(20) << "Name" << "|" << setw(20) << "Username" << "|" << endl;
-		std::cout << "-----------------------------------------------" << endl;
-		for (int i = 0; i < accountHolders.size(); i++)
-		{
-			std::cout << left << setw(20) << accountHolders[i].getName() << "|" << setw(20) << accountHolders[i].getUsername() << "|" << endl;
-		}
-	}
-	void fastForward(int days, vector<bankAccount> &accounts)
-	{
-		//call addInterest() for each account in accounts
-		for (int i = 0; i < accounts.size(); i++)
-		{
-			accounts[i].addInterest(days);
-		}
-	}
+    bankAccountHolder(string name, string username, string password)
+    {
+        this->name = name;
+        this->username = username;
+        this->password = password;
+    }
+    string getName()
+    {
+        return name;
+    }
+    string getUsername()
+    {
+        return username;
+    }
+    string getPassword()
+    {
+        return password;
+    }
+    vector<bankAccount> getAccounts() {
+        return accounts;
+    }
+    void addAccount(bankAccount account)
+    {
+        accounts.push_back(account);
+    }
+    void viewAccounts()
+    {
+        cout << left << setw(20) << "Account Number" << left << setw(20) << "Balance" << left << setw(20) << "Open Date" << left << setw(20) << "Type" << endl;
+        for (auto a : accounts)
+        {
+            cout << left << setw(20) << a.getAccountNumber() << left << setw(20) << a.getBalance() << left << setw(20) << a.getOpenDate() << left << setw(20) << a.getType() << endl;
+        }
+    }
 };
 
-class savingsAccount : public bankAccount {
+class branchManager {
 private:
-	double interestRate;
+    string username;
+    string password;
 public:
-	savingsAccount(string accountNumber, double balance, string openDate, string type, double interestRate) : bankAccount(accountNumber, balance, openDate, type)
-	{
-		this->interestRate = interestRate;
-	}
-	double getInterestRate()
-	{
-		return interestRate;
-	}
-	void setInterestRate(double interestRate)
-	{
-		this->interestRate = interestRate;
-	}
-	void addInterest(int days)
-	{
-		double balance = getBalance();
-		double interest =  balance * interestRate * days / 365;
-		balance += interest;
-		transaction t("Interest", interest, date());
-		addTransaction(t);
-	}
-	string date() {
-		auto t = std::time(nullptr);
-		auto timePtr = std::localtime(&t);
-
-		std::ostringstream oss;
-		oss << std::put_time(timePtr, "%d-%m-%Y");
-		return oss.str();
-	}
+    branchManager(string username, string password) {
+        this->username = username;
+        this->password = password;
+    }
+    string getUsername() {
+        return username;
+    }
+    string getPassword() {
+        return password;
+    }
+    void changeInterestRate(bankAccount &account, int rate) {
+        account.changeInterestRate(rate);
+    }
 };
 
-//current account inherits from bankAccount
-class currentAccount : public bankAccount
-{
-	//has attribute interest rate which is always 0
-	//method getInterestRate() should return 0
-
-public:
-	currentAccount(string accountNumber, double balance, string openDate, string type) : bankAccount(accountNumber, balance, openDate, type)
-	{
-	}
-	void addInterest(int days)
-	{
-	}
-	double getInterestRate()
-	{
-		return 0;
-	}
-};
-
-void register(){
-	string name, username, netBankingPassword;
-	std::cout << "Enter name: ";
-	cin >> name;
-	std::cout << "Enter username: ";
-	cin >> username;
-	std::cout << "Enter netbanking password: ";
-	cin >> netBankingPassword;
-	//check if username already exists
-	//if it does, print "Username already exists"
-	//else, create a new bankAccountHolder object and add it to the accountHolders vector
-	//print "Registration successful"
-	//check in accountHolders
-	for (int i = 0; i < accountHolders.size(); i++)
-	{
-		if (accountHolders[i].username == username)
-		{
-			std::cout << "Username already exists" << endl;
-			return;
-		}
-	}
-	//create a new bankAccountHolder object and add it to the accountHolders vector
-	bankAccountHolder b(name, username, netBankingPassword);
-	accountHolders.push_back(b);
-	std::cout << "Registration successful" << endl;
+void managerMenu(vector<bankAccountHolder> &bank, branchManager manager) {
+    string choice;
+    while (1) {
+        cout << "1. Change interest rate" << endl;
+        cout << "2. Get Statement" << endl;
+        cout << "e. Exit" << endl;
+        cout << "Enter choice: ";
+        cin >> choice;
+        if (choice == "1") {
+            string accountNumber;
+            int changeSuccess = 0;
+            int rate;
+            cout << "Enter account number: ";
+            cin >> accountNumber;
+            cout << "Enter new interest rate: ";
+            cin >> rate;
+            for (int i = 0; i < bank.size(); i++) {
+                for (int j = 0; j < bank[i].getAccounts().size(); j++) {
+                    if (bank[i].getAccounts()[j].getAccountNumber() == accountNumber) {
+                        manager.changeInterestRate(bank[i].getAccounts()[j], rate);
+                        cout << "Interest rate changed successfully" << endl;
+                        changeSuccess = 1;
+                        break;
+                    }
+                }
+            }
+            if (changeSuccess == 0) {
+                cout << "Account not found" << endl;
+            }
+        }
+        else if (choice == "2") {
+            string accountNumber;
+            int accountFound = 0;
+            cout << "Enter account number: ";
+            cin >> accountNumber;
+            for (int i = 0; i < bank.size(); i++) {
+                for (int j = 0; j < bank[i].getAccounts().size(); j++) {
+                    if (bank[i].getAccounts()[j].getAccountNumber() == accountNumber) {
+                        bank[i].getAccounts()[j].viewTransactions();
+                        accountFound = 1;
+                        break;
+                    }
+                }
+            }
+            if (accountFound == 0) {
+                cout << "Account not found" << endl;
+            }
+        }
+        else if (choice == "e") {
+            break;
+        }
+        else {
+            cout << "Invalid choice" << endl;
+        }
+    }
 }
 
-int main(){
-	//object with all the bankAccountHolders
-	vector<bankAccountHolder> accountHolders;
-	int input;
-	//print out options line by line with numbers
-	std::cout << "1. Register" << endl;
-	std::cout << "2. Login" << endl;
-	std::cout << "3. Exit" << endl;
-	cin >> input;
-	if (input == 1)
-	{
-		//register
-		register();
-	}
-	else if (input == 2)
-	{
-		//login
-	}
-	else if (input == 3)
-	{
-		//exit
-	}
-	else
-	{
-		std::cout << "Invalid input" << endl;
-	}
-	return 0;
+void accountHolderMenu(bankAccountHolder &holder) {
+    string choice;
+    while (1) {
+        cout << "1. View accounts" << endl;
+        cout << "2. Deposit" << endl;
+        cout << "3. Withdraw" << endl;
+        cout << "4. View transactions" << endl;
+        cout << "5. Add Account" << endl;
+        cout << "e. Exit" << endl;
+        cout << "Enter choice: ";
+        cin >> choice;
+        if (choice == "1") {
+            holder.viewAccounts();
+        }
+        else if (choice == "2") {
+            string accountNumber;
+            double amount;
+            cout << "Enter account number: ";
+            cin >> accountNumber;
+            cout << "Enter amount to deposit: ";
+            cin >> amount;
+            for (auto &acc : holder.getAccounts()) {
+                if (acc.getAccountNumber() == accountNumber) {
+                    acc.deposit(amount);
+                    cout << "Deposit successful" << endl;
+                    break;
+                }
+            }
+        }
+        else if (choice == "3") {
+            string accountNumber;
+            double amount;
+            cout << "Enter account number: ";
+            cin >> accountNumber;
+            cout << "Enter amount to withdraw: ";
+            cin >> amount;
+            for (auto &acc : holder.getAccounts()) {
+                if (acc.getAccountNumber() == accountNumber) {
+                    acc.withdraw(amount);
+                    break;
+                }
+            }
+        }
+        else if (choice == "4") {
+            string accountNumber;
+            cout << "Enter account number: ";
+            cin >> accountNumber;
+            for (auto &acc : holder.getAccounts()) {
+                if (acc.getAccountNumber() == accountNumber) {
+                    acc.viewTransactions();
+                    break;
+                }
+            }
+        }
+        else if (choice == "5") {
+            string accountNumber;
+            double balance;
+            string openDate;
+            string type;
+            int interestRate;
+            string accountType;
+            cout << "Enter account number: ";
+            cin >> accountNumber;
+            cout << "Enter balance: ";
+            cin >> balance;
+            cout << "Enter open date: ";
+            cin >> openDate;
+            cout << "Enter type: ";
+            cin >> type;
+            cout << "Enter interest rate: ";
+            cin >> interestRate;
+            cout << "Enter account type: ";
+            cin >> accountType;
+            for (auto &acc : holder.getAccounts()) {
+                if (acc.getAccountNumber() == accountNumber) {
+                    cout << "Account number already exists" << endl;
+                    break;
+                }
+            }
+            bankAccount account(accountNumber, balance, openDate, type, interestRate, accountType);
+            holder.addAccount(account);
+            cout << "Account added successfully" << endl;
+        }
+        else if (choice == "e") {
+            break;
+        }
+        else {
+            cout << "Invalid choice" << endl;
+        }
+    }
+}
+
+void login(vector<bankAccountHolder> &bank, branchManager manager) {
+    string username, password;
+    cout << "Enter username: ";
+    cin >> username;
+    cout << "Enter password: ";
+    cin >> password;
+    if (username == manager.getUsername() && password == manager.getPassword()) {
+        managerMenu(bank, manager);
+    }
+    else {
+        for (auto &holder : bank) {
+            if (holder.getUsername() == username && holder.getPassword() == password) {
+                accountHolderMenu(holder);
+                return;
+            }
+        }
+        cout << "Invalid username or password" << endl;
+    }
+}
+
+void signup(vector<bankAccountHolder> &bank, branchManager manager) {
+    //implement signup
+    string name, username, password;
+    cout << "Enter name: ";
+    cin >> name;
+    cout << "Enter username: ";
+    cin >> username;
+    cout << "Enter password: ";
+    cin >> password;
+    bankAccountHolder holder(name, username, password);
+    bank.push_back(holder);
+    cout << "Account created successfully" << endl;
+}
+
+int main() {
+    vector<bankAccountHolder> bank;
+    branchManager manager("manager", "manager");
+
+    string choice;
+    while (1) {
+        cout << "Do you want to login or signup? (login/signup): ";
+        cin >> choice;
+        if (choice == "login") {
+            login(bank, manager);
+        }
+        else if (choice == "signup") {
+            signup(bank, manager);
+        }
+        else {
+            cout << "Invalid choice" << endl;
+        }
+    }
+    return 0;
 }
