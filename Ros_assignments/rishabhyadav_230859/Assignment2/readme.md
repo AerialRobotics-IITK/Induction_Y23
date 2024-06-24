a7f622079f807df3c637b793a4e8c6b58617ba44
rishabh2625 <yadavrishabh709@gmail.com>
2024-06-25 02:37:53 +0530

    ros assignment 1 and 2

---
+++ Ros_assignments/rishabhyadav_230859/Assignment2/readme.md
@@ -0,0 +1,2 @@
+## Assignment 2 
+I have created a scan_subscriber to subscribe to the scan topic of laserscan and then wrote a launch file to include the previous launch file and also include the node to launch rviz
---
+++ Ros_assignments/rishabhyadav_230859/Assignment2/smb_highlevel_controller_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/root/Workspaces/smb_highlevel_controller_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/root/Workspaces/smb_highlevel_controller_ws/devel;/opt/ros/noetic'.split(';') if '/root/Workspaces/smb_highlevel_controller_ws/devel;/opt/ros/noetic' != '' else []
